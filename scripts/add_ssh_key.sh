a55a8a9936710ec2a7367d5b4c9b18fb94271c80
Justin <jrussel1055@gmail.com>
2023-03-14 11:47:57 -0500

    Don't add pubkey to authorized_keys if already exists

--- scripts/add_ssh_key.sh
+++ scripts/add_ssh_key.sh
@@ -3,10 +3,14 @@
 # $1 of the volume
 # $2 ssh pubkey
 
-# write ssh pubkey to ~/.ssh/authorized_keys
 function add_volume_pubkey(){
+    # check if pubkey already exists in authorized_keys
+    if grep -q "$2" ~/.ssh/authorized_keys; then
+        exit 0
+    fi
+
+    # write ssh pubkey to ~/.ssh/authorized_keys
     echo "command=\"borg serve --restrict-to-path /volumes/$1\",restrict $2" >> ~/.ssh/authorized_keys
 }
-
 
 add_volume_pubkey $1 "$2"
