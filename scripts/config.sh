0e7f12f10b0a032ff50a1c15eb8e751cf964de24
Mo Balaa <balaa@fractalnetworks.co>
2023-03-30 07:46:08 -0500

    use bash impl of crudini, thats gtp4

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
+set -x
 
-function get_config() {
-    echo $(crudini --get $1 $2 $3)
-}
+file=$2
+section=$3
+key=$4
+value=$5
 
-
-function set_config() {
-    crudini --set $1 $2 $3 $4
-}
+if [[ "$1" == "set" ]]; then
+    if ! grep -q "^\[$section\]" "$file"; then
+        echo "[$section]" >> "$file"
+    fi
+    if grep -q "^\[$section\]" "$file"; then
+        if grep -qE "^\s*$key\s*=" "$file"; then
+            sed -i -E "s/(^\s*$key\s*=).*/\1$value/" "$file"
+        else
+            sed -i "/^\[$section\]/a $key=$value" "$file"
+        fi
+    fi
+elif [[ "$1" == "get" ]]; then
+    if grep -q "^\[$section\]" "$file"; then
+        value=$(awk -F= -v section="$section" -v key="$key" '
+            /^\[.*\]$/ { in_section = (/^\['"$section"'\]$/) }
+            in_section && $1 ~ /^'"$key"'$/ { print $2; exit }
+        ' "$file")
+        if [[ -n "$value" ]]; then
+            echo "$value"
+        else
+            echo "Key not found"
+            exit 1
+        fi
+    else
+        echo "Section not found"
+        exit 1
+    fi
+else
+    echo "Invalid command. Use 'set' or 'get'."
+    exit 1
+fi
--- scripts/remote/add.sh
+++ scripts/remote/add.sh
@@ -4,12 +4,13 @@ SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 source $SCRIPT_DIR/../base.sh
 
-
+echo "SEEEE"
+echo "$@"
 # make sure 2 arguments are passed, else exit
 if [ $# -ne 2 ]; then
     echo "usage: s4 remote add <name> <url>"
     exit 1
 fi
 
-set_config .s4/config remotes $1 $2
+s4 config set .s4/config remotes $1 $2
 
